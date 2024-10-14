b01585dec1106ebdee6048b095e2ddd0a5108a01
Karthi-47 <karthippm10@gmail.com>
2024-10-14 23:41:54 +0530

    Front Page Created

---
+++ portal/src/components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <section class="pt-10 bg-gray-100 sm:pt-16 lg:pt-6">
+    <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="max-w-2xl mx-auto text-center">
+            <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">Real humans are here to help you building your brand</h2>      
+        </div>
+    </div>
+
+    <div class="container mx-auto 2xl:px-12">
+        <img class="w-full mt-6" src="https://cdn.rareblocks.xyz/collection/celebration/images/team/4/group-of-people.png" alt="" />
+    </div>
+</section>
+
+)}
+export default Content
