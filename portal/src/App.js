a49f6e6226a42b3ca16ef3b5de8fc59e79f8a1b7
Karthi-47 <karthippm10@gmail.com>
2024-10-15 17:19:18 +0530

    Login Popup Added

--- portal/src/App.js
+++ portal/src/App.js
@@ -1,16 +1,34 @@
+import React, { useState } from "react";
 import './App.css';
-import Header from './components/Header'
-import Content from './components/Content';
-import Footer from './components/Footer';
+import Header from "./components/Header"; 
+import Login from "./components/Login";    
+import Content from "./components/Content";
+import Footer from "./components/Footer"
 
-function App() {
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  // Function to open the login popup
+  const handleOpenPopup = () => {
+    setShowPopup(true);
+  };
+
+  // Function to close the login popup
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return (
-    <div>
-        <Header />
-        <Content />
-        <Footer />
+    <div className="App">
+      {/* Passing the openPopup function to the Header */}
+      <Header openPopup={handleOpenPopup} />
+      
+      {/* Rendering the Login component and passing the closePopup function */}
+      {showPopup && <Login closePopup={handleClosePopup} />}
+      <Content />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- portal/src/components/Header.js
+++ portal/src/components/Header.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({openPopup}) => {
+
     const  [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
+    
   return (
     <header class="pb-6 bg-white lg:pb-0">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -10,7 +11,7 @@ const Header = () => {
         <nav class="flex items-center justify-between h-16 lg:h-20">
             <div class="flex-shrink-0">
                 <a href="#" title="" class="flex">
-                    <img class="w-auto h-8 lg:h-10" src="https://cdn.rareblocks.xyz/collection/celebration/images/logo.svg" alt="" />
+                    <h1 className="font-bold text-2xl">Alumini Portal</h1>
                 </a>
             </div>
 
@@ -27,34 +28,34 @@ const Header = () => {
             </button>
 
             <div class="hidden lg:flex lg:items-center lg:ml-auto lg:space-x-10">
-                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Features </a>
+                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Solutions </a>
+                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Resources </a>
+                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Pricing </a>
+                <a href="#" title="" class="text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
             </div>
 
-            <a href="#" title="" class="items-center justify-center hidden px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md lg:inline-flex hover:bg-blue-700 focus:bg-blue-700" role="button"> Login Now </a>
-        </nav>
+            <button onClick={openPopup} class="hidden md:block inline-flex justify-center px-4 py-3 ml-10 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700">Login Now</button>        
+            </nav>
 
         {/* <!-- xs to lg --> */}
         {isMenuOpen && <nav class="pt-4 pb-6 bg-white border border-gray-200 rounded-md shadow-md lg:hidden">
             <div class="flow-root">
                 <div class="flex flex-col px-6 -my-2 space-y-1">
-                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Features </a>
+                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Solutions </a>
+                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Resources </a>
+                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
 
-                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Pricing </a>
+                    <a href="#" title="" class="inline-flex py-2 text-base font-medium text-black transition-all duration-200 hover:text-blue-600 focus:text-blue-600"> Menu </a>
                 </div>
             </div>
 
             <div class="px-6 mt-6">
-                <a href="#" title="" class="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700" role="button"> Login Now </a>
+            <button onClick={openPopup} class="inline-flex justify-center px-4 py-3 text-base font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md tems-center hover:bg-blue-700 focus:bg-blue-700">Login Now</button>            
             </div>
         </nav>}
     </div>
